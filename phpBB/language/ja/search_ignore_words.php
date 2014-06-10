b1ce1002ff775ab48a4dbd2117309473e78240ef
ntth <Yasuhiro.Nattoh@jrits.ricoh.co.jp>
2014-06-10 15:22:44 +0900

    Language Pack　の日本語を追加

---
+++ phpBB/language/ja/search_ignore_words.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*
+* search_ignore_words [Japanese]
+*
+* @package language
+* @version $Id$
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+$words = array(
+);
+
+?>
